1778139e19ebf648c2db72d47f365f60c5394320
spider5454 <makwanap988@gmail.com>
2024-05-05 13:39:42 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Products from './Components/Products'
+
+function App() {
+  return (
+    <>
+      
+      <div className="bg-slate-300">
+      <Products/>
+    </div>
+    </>
+  )
+}
+
+export default App
